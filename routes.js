0b175ce8b616174e8ec899ffda636398dc9701f5
Voya <voya@voyacode.com>
2017-06-25 18:13:20 +0300

    Add initial node apis for blogs and comments

--- routes.js
+++ routes.js
@@ -1,8 +1,24 @@
+const request = require('request');
+
 module.exports = function(app) {
 
 	app.get('/api/test', function(req, res) {
     res.json({test: "This is an api test"})
 	});
+
+  app.get('/api/blogs', function(req, res) {
+    const uri = 'http://69.5.14.14/php/blogs.php' + (req.query.limit ? '?limit=' + req.query.limit : '');
+    request({
+      uri: uri
+    }).pipe(res);
+	});
+
+  app.get('/api/comments', function(req, res){
+    const uri = 'http://69.5.14.14/php/getComments.php';
+    request({
+      uri: uri
+    }).pipe(res);
+  })
 
 	app.get('*', function(req, res) {
     console.log(res);
