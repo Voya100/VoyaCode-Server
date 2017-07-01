a54d1f2ea598c10f1a18838e9937811cc812930a
Voya <voya@voyacode.com>
2017-07-01 16:11:41 +0300

    Add parsing and miniauthentication to blogs

--- api/blogs/blogs.js
+++ api/blogs/blogs.js
@@ -1,9 +1,11 @@
 var db = require('../../database');
+var moment = require('moment-timezone');
 
 function getBlogs(req, res, next) {
   var limit = req.query.limit ? 'LIMIT ' + req.query.limit : '';
   db.any('select * from blogs order by id DESC $1^', limit)
     .then(function (data) {
+      data.forEach(parseBlogData);
       res.status(200)
         .json({
           status: 'success',
@@ -20,6 +22,7 @@ function getBlog(req, res, next) {
   var id = parseInt(req.params.id);
   db.one('select * from blogs where id = $1', id)
     .then(function (data) {
+      parseBlogData(data);
       res.status(200)
         .json({
           status: 'success',
@@ -31,7 +34,13 @@ function getBlog(req, res, next) {
     });
 }
 
+function parseBlogData(data){
+  data.date = moment(data.date).tz('Europe/Helsinki').format('DD.MM.YYYY');
+}
+
 function addBlog(req, res, next) {
+  if(req.body.secret != 'blog-secret') return next();
+  parseBlogContent(req.body);
   db.none('insert into blogs(name, text)' +
       'values(${name}, ${text})',
     req.body)
@@ -47,7 +56,13 @@ function addBlog(req, res, next) {
     });
 }
 
+function parseBlogContent(data){
+  data.text = data.text.replace(/\n/g, "<br>");
+}
+
 function updateBlog(req, res, next) {
+  if(req.body.secret != 'blog-secret') return next();
+  parseBlogContent(req.body);
   db.none('update blogs set name=$1, text=$2 where id=$3',
     [req.body.name, req.body.text, parseInt(req.params.id)])
     .then(function () {
