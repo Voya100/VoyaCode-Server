af6da70adb494f676b6f92a0402fca2dba2e3cb4
Voya <voya@voyacode.com>
2017-09-03 18:24:24 +0300

    Add tag handling to blogs

--- api/blogs/blogs.js
+++ api/blogs/blogs.js
@@ -1,45 +1,49 @@
-var db = require('../../database');
-var moment = require('moment-timezone');
+module.exports = {
+  getBlogs,
+  getBlog,
+  addBlog,
+  updateBlog,
+  deleteBlog,
+  previewBlog
+};
+
+const db = require('../../database');
+const formatter = require('../shared/formatter.service');
 
 function getBlogs(req, res, next) {
-  var limit = req.query.limit ? 'LIMIT ' + req.query.limit : '';
+  const limit = req.query.limit ? 'LIMIT ' + req.query.limit : '';
   db.any('select * from blogs order by id DESC $1^', limit)
     .then(function (data) {
-      data.forEach(parseBlogData);
+      data.forEach(formatBlogResponse);
       res.status(200)
         .json({
           status: 'success',
           data: data
         });
     })
-    .catch(function (err) {
-      console.log(err);
-      return next(err);
-    });
+    .catch(next);
 }
 
 function getBlog(req, res, next) {
-  var id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
   db.one('select * from blogs where id = $1', id)
     .then(function (data) {
-      parseBlogData(data);
+      formatBlogResponse(data);
       res.status(200)
         .json({
           status: 'success',
           data: data
         });
     })
-    .catch(function (err) {
-      return next(err);
-    });
+    .catch(next);
 }
 
-function parseBlogData(data){
-  data.date = moment(data.date).tz('Europe/Helsinki').format('DD.MM.YYYY');
+function formatBlogResponse(blog){
+  blog.date = formatter.formatDate(blog.date);
+  blog.text = formatter.tagsToHtml(blog.text);
 }
 
 function addBlog(req, res, next) {
-  parseBlogContent(req.body);
   db.none('insert into blogs(name, text)' +
           'values(${name}, ${text})', req.body)
     .then(function () {
@@ -48,18 +52,10 @@ function addBlog(req, res, next) {
           status: 'success'
         });
     })
-    .catch(function (err) {
-      console.log(err);
-      return next(err);
-    });
-}
-
-function parseBlogContent(data){
-  data.text = data.text.replace(/\n/g, "<br>");
+    .catch(next);
 }
 
 function updateBlog(req, res, next) {
-  parseBlogContent(req.body);
   db.none('update blogs set name=$1, text=$2 where id=$3',
     [req.body.name, req.body.text, parseInt(req.params.id)])
     .then(function () {
@@ -79,16 +75,6 @@ function deleteBlog(req, res, next){
 
 function previewBlog(req, res, next){
   var blog = {name: req.body, text: req.body, id: req.body.id || 1, date: new Date()};
-  parseBlogContent(blog);
-  parseBlogData(blog);
-  res.status(200).json(blog);
+  formatBlogResponse(blog);
+  res.status(200).json({status: 'success', data: blog});
 }
-
-module.exports = {
-  getBlogs,
-  getBlog,
-  addBlog,
-  updateBlog,
-  deleteBlog,
-  previewBlog
-};
