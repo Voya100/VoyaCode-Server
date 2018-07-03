1616c54f2fed7c58bc4324e33f117919dfdabaab
Voya <voya@voyacode.com>
2018-07-03 20:28:42 +0300

    Modify app's bootstrap and add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { logger: console });
+
+  app.use(morgan('dev'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      skipMissingProperties: true,
+      validationError: {
+        target: false,
+        value: false
+      }
+    })
+  );
+  app.useStaticAssets(join(__dirname, '../../public'));
+
+  await app.listen(process.env.port || 8080);
 }
 bootstrap();
