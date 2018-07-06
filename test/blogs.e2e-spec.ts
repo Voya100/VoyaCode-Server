f44796a913f26ead37a051b08df7ba553942df8f
Voya <voya@voyacode.com>
2018-07-06 17:17:40 +0300

    Fix e2e tests after the id fix and add tests for

--- test/blogs.e2e-spec.ts
+++ test/blogs.e2e-spec.ts
@@ -1,5 +1,4 @@
 import { Blog } from '@api/blogs/blog.entity';
-import { BlogResult } from '@api/blogs/blogs.interfaces';
 import { INestApplication } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import request from 'supertest';
@@ -20,38 +19,34 @@ describe('BlogController (e2e)', () => {
     blogRepository.clear();
   });
 
-  const rawBlogs: Blog[] = [
+  const rawBlogs = [
     {
-      id: 1,
       name: 'Blog1',
       text: 'Blog 1 [b]description[/b]',
       date: new Date('2018-07-01T19:22:10.152Z')
     },
     {
-      id: 2,
       name: 'Blog2',
       text: 'Blog 2 description',
       date: new Date('2018-07-11T19:22:10.152Z')
     }
   ];
-  const formattedBlogs: BlogResult[] = [
+  const formattedBlogs = [
     {
-      id: 2,
-      name: 'Blog2',
-      text: 'Blog 2 description',
-      date: '11.07.2018',
-      year: 2018
-    },
-    {
-      id: 1,
       name: 'Blog1',
       text: 'Blog 1 <b>description</b>',
       date: '01.07.2018',
       year: 2018
+    },
+    {
+      name: 'Blog2',
+      text: 'Blog 2 description',
+      date: '11.07.2018',
+      year: 2018
     }
   ];
 
-  describe('/GET /api/blogs', () => {
+  describe('GET /api/blogs', () => {
     it('should return empty array of blogs', () => {
       return request(app.getHttpServer())
         .get('/api/blogs')
@@ -60,15 +55,18 @@ describe('BlogController (e2e)', () => {
     });
 
     it('should return formatted blog results in descending ID order', async () => {
-      await blogRepository.insert(rawBlogs);
+      const inserts = await blogRepository.insert(rawBlogs);
+      const expectedData = inserts.identifiers
+        .map((id, i) => ({ ...id, ...formattedBlogs[i] }))
+        .reverse();
       return request(app.getHttpServer())
         .get('/api/blogs')
         .expect(200)
-        .expect({ data: formattedBlogs });
+        .expect({ data: expectedData });
     });
   });
 
-  describe('/GET /api/blogs/:id', () => {
+  describe('GET /api/blogs/:id', () => {
     it('should return 404 when id does not exist', () => {
       return request(app.getHttpServer())
         .get('/api/blogs/1')
@@ -82,11 +80,12 @@ describe('BlogController (e2e)', () => {
 
     it('should return formatted blog result', async () => {
       await blogRepository.insert(rawBlogs);
+      const blog = await blogRepository.findOne(rawBlogs[0]);
       return request(app.getHttpServer())
-        .get('/api/blogs/1')
+        .get('/api/blogs/' + blog.id)
         .expect(200)
         .expect({
-          data: formattedBlogs[1]
+          data: { id: blog.id, ...formattedBlogs[0] }
         });
     });
   });
