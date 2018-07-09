f70e62254517df3ba7ffaaf9c5f8d8ee0bbc8a09
Voya <voya@voyacode.com>
2018-07-09 20:53:44 +0300

    Add CacheService unit tests

--- src/core/cache.service.spec.ts
+++ src/core/cache.service.spec.ts
@@ -1,16 +1,73 @@
 import { CacheService } from '@core/cache.service';
-import { CoreModule } from '@core/core.module';
-import { Test, TestingModule } from '@nestjs/testing';
 
 describe('CacheService', () => {
   let service: CacheService;
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [CoreModule]
-    }).compile();
-    service = module.get<CacheService>(CacheService);
+  let cache: any;
+  beforeEach(async () => {
+    cache = {};
+    service = new CacheService(cache);
   });
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // Testing that callbacks are mapped to Promise correctly
+  describe('get', () => {
+    it('should return resolved promise', () => {
+      cache.get = jest
+        .fn()
+        .mockImplementation((_, fn: (err: any, value: any) => void) => {
+          fn(false, 'value');
+        });
+      expect(service.get('key')).resolves.toBe('value');
+    });
+
+    it('should return rejected promise', () => {
+      cache.get = jest
+        .fn()
+        .mockImplementation((_, fn: (err: any, value: any) => void) => {
+          fn('error-message', false);
+        });
+      expect(service.get('key')).rejects.toBe('error-message');
+    });
+  });
+
+  describe('set', () => {
+    it('should return resolved promise', async () => {
+      cache.set = jest
+        .fn()
+        .mockImplementation(
+          (
+            _key: string,
+            _value: any,
+            _ttl: number,
+            fn: (err: any, success: any) => void
+          ) => {
+            fn(false, true);
+          }
+        );
+      await expect(service.set('key', 'value')).resolves.toBe(true);
+    });
+
+    it('should return rejected promise', async () => {
+      cache.set = jest
+        .fn()
+        .mockImplementation(
+          (_key, _value, _ttl, fn: (err: any, value: any) => void) => {
+            fn('error-message', false);
+          }
+        );
+      await expect(service.set('key', 'value')).rejects.toBe('error-message');
+    });
+  });
+
+  describe('delete', () => {
+    it('should return resolved promise', async () => {
+      cache.del = jest.fn().mockImplementation((_key, callback) => {
+        callback();
+      });
+      // tslint:disable-next-line:no-unused-expression
+      await expect(service.delete('key')).resolves;
+    });
   });
 });
